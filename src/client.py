b62085698f6a6ce63a2e932651501786c520abff
Dennis <karaktaka@users.noreply.github.com>
2025-12-20 20:49:05 +0100

    Add retry logic for Tado API connection and improve error handling

--- src/client.py
+++ src/client.py
@@ -9,6 +9,7 @@ import logging
 from os import getenv
 from time import sleep
 
+import requests.exceptions
 from libtado.api import Tado
 from prometheus_client import Gauge, start_http_server
 from requests.exceptions import HTTPError
@@ -25,12 +26,12 @@ ACTIVITY_AC_POWER = Gauge(
 )
 SETTING_TEMPERATURE = Gauge(
     "tado_setting_temperature_value",
-    "The temperature of a specific zone in celsius degres.",
+    "The temperature of a specific zone in celsius degrees.",
     ["zone", "type", "unit"],
 )
 SENSOR_TEMPERATURE = Gauge(
     "tado_sensor_temperature_value",
-    "The temperature of a specific zone in celsius degres",
+    "The temperature of a specific zone in celsius degrees",
     ["zone", "type", "unit"],
 )
 SENSOR_HUMIDITY_PERCENTAGE = Gauge(
@@ -66,38 +67,63 @@ def set_logging_level(_level, _logger=None):
     return _logger
 
 
+def retry(_header, _max_retries: int):
+    _retry_after = None
+    if _max_retries <= 0:
+        log.error("Maximum retries reached. Exiting.")
+        return False
+    _rate_limit_header = _header.get("RateLimit", "")
+    for _part in _rate_limit_header.split(";"):
+        if _part.strip().startswith("t="):
+            _retry_after = int(_part.strip()[2:])
+    if _retry_after:
+        log.error(f"Tado API rate limit exceeded. Retrying after {_retry_after} seconds.")
+        sleep(_retry_after)
+        _max_retries -= 1
+    return True, _max_retries
+
+
 if __name__ == "__main__":
-    # Start up the server to expose the metrics.
+    tado = None
+
+    # Read configuration from environment variables
     temperature_unit = getenv("TADO_TEMPERATURE_UNIT", "celsius")
     refresh_rate = int(getenv("TADO_EXPORTER_REFRESH_RATE", 30))
+    max_retries = int(getenv("TADO_API_MAX_RETRIES", 5))
     loglevel = getenv("LOGLEVEL", "INFO")
 
     # set logging level
     log = set_logging_level(loglevel)
 
+    # Connect to Tado API
+    log.info("Connecting to tado API...")
+    while True:
+        try:
+            tado = Tado(token_file_path="refresh_token.json")
+            status = tado.get_device_activation_status()
+
+            if status == "PENDING":
+                tado.device_activation()
+            log.info("Connected to Tado API")
+            break
+        except (KeyError, HTTPError) as error:
+            if error.response.status_code == requests.codes.too_many_requests:
+                retry, max_retries = retry(error.response.headers, max_retries)
+                if retry:
+                    continue
+                exit(1)
+            elif (
+                isinstance(error, KeyError)
+                or [requests.codes.unauthorized, requests.codes.forbidden] in error.response.status_code
+            ):
+                log.error("Authentication failed. Please check your credentials.")
+            exit(2)
+
+    # Start the Prometheus metrics server
     log.info("Starting tado exporter")
     start_http_server(8000)
-
-    log.info("Connecting to tado API...")
-    try:
-        tado = Tado(token_file_path="refresh_token.json")
-
-        status = tado.get_device_activation_status()
-
-        if status == "PENDING":
-            url = tado.get_device_verification_url()
-            tado.device_activation()
-            status = tado.get_device_activation_status()
-    except (KeyError, HTTPError) as error:
-        log.error("Authentication failed.")
-        log.debug(error)
-        raise
-    else:
-        log.info("Connected to Tado API")
-
     log.info("Exporter ready")
     while True:
-        # noinspection PyBroadException
         try:
             for zone in tado.get_zones():
                 data = tado.get_state(zone["id"])
@@ -127,6 +153,13 @@ if __name__ == "__main__":
                 SENSOR_WINDOW_OPENED.labels(zone["name"], zone["type"]).set(
                     1 if tado.get_state(zone["id"])["openWindow"] is not None else 0
                 )
+        except HTTPError as error:
+            if error.response.status_code == requests.codes.too_many_requests:
+                retry, max_retries = retry(error.response.headers, max_retries)
+                if retry:
+                    continue
+                exit(1)
+            log.debug(error)
         except Exception as error:
             log.error("Cannot read data from Tado API. Will retry later.")
             log.debug(error)
